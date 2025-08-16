28c43e7cadda479a69f26e35a402914cc8890ff7
Charmaine Lee <charmainelee011@gmail.com>
2025-08-16 16:47:55 -0400

    testing

---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import './components/Sidebar.css';
+
+function Sidebar() {
+    return (
+
+        <div>
+            <h1 className = "pfp"> temp </h1>
+            <h1> Charmaine Lee </h1>
+
+            <nav>
+                <a href = '#'> Home </a>
+                <a href = '#'> My Recipes </a>
+                <a href = '#'> Favourites </a>
+                <a href = '#'> About me </a>
+
+            </nav>
+
+        </div>
+
+
+    );
+}
+
+export default Sidebar;
